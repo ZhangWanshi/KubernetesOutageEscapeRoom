0f0ffd7991c59ce3f25bec1a0461d3e19796e871
ZhangWanshi <wanshizhang77@gmail.com>
2026-05-28 12:06:01 +0100

    feat: initial commit

---
+++ src/main/java/com/team5/kubernetesoutageescaperoom/KubernetesOutageEscapeRoomApplication.java
@@ -0,0 +1,13 @@
+package com.team5.kubernetesoutageescaperoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesOutageEscapeRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubernetesOutageEscapeRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team5/kubernetesoutageescaperoom/KubernetesOutageEscapeRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team5.kubernetesoutageescaperoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesOutageEscapeRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
